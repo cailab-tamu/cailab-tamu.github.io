f80b6be0dc1c03dcb507112745a7906f6cb14078
James Cai <jamescai@genomezoo.net>
2024-08-07 14:59:03 -0500

    .

---
+++ src/pmidoi/aaa.m
@@ -0,0 +1,14 @@
+function s=aaa(a,tag)
+    s='';
+    tag=sprintf('<meta name="%s" content="',tag);
+    idx=find(contains(a,tag));
+    if isempty(idx), return; end
+    %length(idx)
+    s=strings(length(idx),1);
+    for k=1:length(idx)
+        b=a{idx(k)};
+        b=b(strfind(b, tag)+strlength(tag):end);
+        b=b(1:strfind(b,'"')-1);
+        s(k)=b;  
+    end
+end
