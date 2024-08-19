6a2b67d17eb34859dc6c255a05f7e26604fbc8ea
jamesjcai <jamescai@genomezoo.net>
2024-08-19 07:42:04 -0500

    .

--- src/pmidoi/e_authornamemod.m
+++ src/pmidoi/e_authornamemod.m
@@ -7,6 +7,7 @@ function [s_au]=e_authornamemod(s_au)
     % "Cai, James J."
 for k = 1:length(s_au)
     idx = strfind(s_au(k),',');
+    if isempty(idx), continue; end
     surnm = extractBefore(s_au(k), idx);
     fstnm = strtrim(extractAfter(s_au(k), strfind(s_au(k),',')));
     fstin = regexp(fstnm, '[A-Z]', 'match');
